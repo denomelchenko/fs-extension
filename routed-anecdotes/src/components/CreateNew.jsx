873b2be4edf26d1875beb77b7e49970597e980e8
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 21:01:28 +0200

    part7: exercise 7.2 useField with reset

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -18,6 +18,12 @@ const CreateNew = ({ addNew }) => {
     navigate('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -35,6 +41,9 @@ const CreateNew = ({ addNew }) => {
           <input name="info" {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   )
